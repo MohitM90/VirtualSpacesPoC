0baaae455ce306dc901a47f3f7c500cde31c5fec
Mohit Makhija <mohit.makhija@avanade.com>
2021-11-17 08:59:11 +0100

    Add project files.

---
+++ VirtualSpaces.API/Controllers/GraphController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VirtualSpaces.API.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+    [AuthorizeForScopes(Scopes = new[] { "user.read" })]
+    public class GraphController : ControllerBase
+    {
+        GraphServiceClient _graphServiceClient;
+
+        public GraphController(GraphServiceClient graphServiceClient)
+        {
+            _graphServiceClient = graphServiceClient;
+        }
+
+        // GET: /<GraphController>
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            try
+            {
+                var user = await _graphServiceClient.Me.Request().GetAsync();
+                return user.DisplayName;
+            }
+            catch (Exception e)
+            {
+                return "error";
+            }
+        }
+
+    }
+}
---
+++ VirtualSpaces.WEB/Pages/Index.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace VirtualSpaces.WEB.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public string MessageMyApi { get; private set; } = "PageModel in C#";
+        public string Token { get; private set; } = "";
+
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ITokenAcquisition _tokenAcquisition;
+        private readonly IConfiguration _configuration;
+
+        private readonly string baseUrl;
+
+        public IndexModel(ILogger<IndexModel> logger, ITokenAcquisition tokenAcquisition, IConfiguration configuration)
+        {
+            _logger = logger;
+            _tokenAcquisition = tokenAcquisition;
+            _configuration = configuration;
+
+            baseUrl = _configuration.GetValue<string>("VirtualSpacesBackend:BaseUrl");
+        }
+
+        public async Task OnGet()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                var accessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(_configuration.GetValue<string>("VirtualSpacesBackend:Scopes")?.Split(' '));
+                Token = accessToken; // Pass Token to ViewModel to show it on the View
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                var data = await client.GetStringAsync($"{baseUrl}/Graph");
+                MessageMyApi = data; // Pass Response of REST call to ViewModel to show it on the View
+            } catch (Exception e)
+            {
+                MessageMyApi = e.Message + " | " + e.InnerException;
+            }
+
+        }
+        
+    }
+}
